7a3d094f3269ac4f0675e568369f444f846bd648
Varun <varun769097@gmail.com>
2023-12-01 06:28:36 +0530

    changed img

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { proxy } from "valtio";
-
+import img from "../assets/blacklogo.png"
 const state = proxy({
     intro: true,
     color: '#ffffff',
     isLogoTexture: true,
     isFullTexture: false,
-    logoDecal: '../../blacklogo.png',
-    fullDecal: '../../blacklogo.png',
+    logoDecal: img,
+    fullDecal: img,
 });
 
 export default state;
